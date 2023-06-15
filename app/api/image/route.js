a8df2c84afb835b547e8a6bc06ecccc38e6df1c0
busma13 <peterluitjens@gmail.com>
2023-06-14 21:21:04 -0700

    Begin User interface.

--- app/api/image/route.js
+++ app/api/image/route.js
@@ -1,18 +1,19 @@
-import { Configuration, OpenAIApi } from "openai";
-import { NextResponse } from "next/server";
+import { Configuration, OpenAIApi } from "openai"
+import { NextResponse } from "next/server"
 const configuration = new Configuration({
-    organization: process.env.OPENAI_ORGANIZATION,
-    apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+  organization: process.env.OPENAI_ORGANIZATION,
+  apiKey: process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(configuration)
 
 export async function GET(request) {
-const { prompt } = JSON.parse(request.body)
+  const { prompt } = JSON.parse(request.body)
+  console.log('prompt: ', prompt)
 
   const response = await openai.createImage({
     prompt,
-    n:1,
-    size: '512x512'
-  });
-  return NextResponse.json({ data: response.data.data });
+    n: 1,
+    size: "512x512",
+  })
+  return NextResponse.json({ image: response.data.data[0].url })
 }
